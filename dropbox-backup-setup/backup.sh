b381f0b4191c905d58aab22386889a750565ecc5
Shubham Trivedi <99660331+deploywithshubham@users.noreply.github.com>
2022-06-22 19:53:12 +0530

    Update backup.sh

--- dropbox-backup-setup/backup.sh
+++ dropbox-backup-setup/backup.sh
@@ -6,5 +6,5 @@ mkdir dropbox
 cd dropbox
 curl "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh" -o dropbox-uploader.sh
 chmod +x dropbox-uploader.sh
-curl "https://raw.githubusercontent.com/maskoid/vestacustom/master/backup/send_site_backup_files_to_dropbox.sh" -o /usr/local/hestia/bin/push-to-dropbox.sh
+curl "https://raw.githubusercontent.com/deploywithshubham/hestiacp/main/dropbox-backup-setup/push-to-dropbox.sh" -o /usr/local/hestia/bin/push-to-dropbox.sh
 chmod 755 /usr/local/hestia/bin/push-to-dropbox.sh
